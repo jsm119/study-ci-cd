68f6cad8dbf6663492edd20642518ec8a9d7c7a3
sokuli-bit <85813876+sokuli-bit@users.noreply.github.com>
2023-10-24 13:38:23 +0900

    Test github actions

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import os
-
-import openai
 from flask import Flask, redirect, render_template, request, url_for
+import os
+import openai
+
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=("GET", 
+"POST"))
 def index():
     if request.method == "POST":
         characteristic = request.form["characteristic"]
         response = openai.Completion.create(
-            model="text-davinci-003",
+                    model="text-davinci-003",
             prompt=generate_prompt(characteristic),
             temperature=0.6,
         )
