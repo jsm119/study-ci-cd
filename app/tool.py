3e8c6f957708a9daee2f724b06eb2f0d42955f8d
GitHub Action <action@github.com>
2023-10-26 07:30:07 +0000

    Automated format fixes

--- app/tool.py
+++ app/tool.py
@@ -1,9 +1,11 @@
-
-import os
 import math
+import os
+
 
 def sub(a, b) -> int:
     return math.floor(a - b)
+
+
 def word_count(sentence, word) -> int:
     sentence = sentence.lower().split()
     if word in sentence:
